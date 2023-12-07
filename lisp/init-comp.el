b22e0c619525d5a519fa9236623259e05f2c386c
Samray <samray.tian@gmail.com>
2023-12-08 00:15:43 +0800

    Add Vertico for better minibuffer completion

--- lisp/init-comp.el
+++ lisp/init-comp.el
@@ -12,5 +12,17 @@
 
 ;;; Code:
 
+;; Vertico
+;; A better minibuffer interactive completion system
+;; link: https://github.com/minad/vertico
+(use-package vertico
+  :init
+  (vertico-mode)
+  :config
+  (setq vertico-cycle t)
+  (setq vertico-count 20)
+  (setq vertico-resize t)
+  (setq enable-recursive-minibuffers t))
+
 (provide 'init-comp)
 ;; init-comp.el ends here
