5b38442957260ea7560bc74f91ad37367137786a
Samray <samray.tian@gmail.com>
2023-12-09 23:20:12 +0800

    Add use-package-always-defer option

--- lisp/init-paclage.el
+++ lisp/init-paclage.el
@@ -34,6 +34,7 @@
 ;; Configure use-package
 (eval-and-compile
   (setq use-package-always-ensure t)
+  (setq use-package-always-defer t)
   (setq use-package-expand-minimally t)
   (setq use-package-enable-imenu-support t)
   (setq use-package-compute-statistics t)
